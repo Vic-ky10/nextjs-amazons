a6dfc5cd8d6a9b62e53b98680d45a73c1a7254c1
vigneshwaran <vigneshvenketasan@gmail.com>
2026-05-01 11:07:11 +0530

    06-create-todays-deals-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,30 +1,32 @@
-import { HomeCard } from '@/components/shared/home/home-card'
-import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import { HomeCard } from "@/components/shared/home/home-card";
+import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import ProductSlider from "@/components/shared/product/product-slider";
+import { Card, CardContent } from "@/components/ui/card";
+import { getAllCategories, getProductsByTag, getProductsForCard , } from "@/lib/actions/product.actions";
+import data from "@/lib/data";
+import { toSlug } from "@/lib/utils";
 
-import { getAllCategories, getProductsForCard } from '@/lib/actions/product.actions'
-import data from '@/lib/data'
-import { toSlug } from '@/lib/utils'
 
 export default async function HomePage() {
-  const categories = (await getAllCategories()).slice(0, 4)
+  const categories = (await getAllCategories()).slice(0, 4);
   const newArrivals = await getProductsForCard({
-    tag: 'new-arrival',
+    tag: "new-arrival",
     limit: 4,
-  })
+  });
   const featureds = await getProductsForCard({
-    tag: 'featured',
+    tag: "featured",
     limit: 4,
-  })
+  });
   const bestSellers = await getProductsForCard({
-    tag: 'best-seller',
+    tag: "best-seller",
     limit: 4,
-  })
+  });
   const cards = [
     {
-      title: 'Categories to explore',
+      title: "Categories to explore",
       link: {
-        text: 'See More',
-        href: '/search',
+        text: "See More",
+        href: "/search",
       },
       items: categories.map((category) => ({
         name: category,
@@ -33,37 +35,45 @@ export default async function HomePage() {
       })),
     },
     {
-      title: 'Explore New Arrivals',
+      title: "Explore New Arrivals",
       items: newArrivals,
       link: {
-        text: 'View All',
-        href: '/search?tag=new-arrival',
+        text: "View All",
+        href: "/search?tag=new-arrival",
       },
     },
     {
-      title: 'Discover Best Sellers',
+      title: "Discover Best Sellers",
       items: bestSellers,
       link: {
-        text: 'View All',
-        href: '/search?tag=new-arrival',
+        text: "View All",
+        href: "/search?tag=new-seller",
       },
     },
     {
-      title: 'Featured Products',
+      title: "Featured Products",
       items: featureds,
       link: {
-        text: 'Shop Now',
-        href: '/search?tag=new-arrival',
+        text: "Shop Now",
+        href: "/search?tag=new-featured",
       },
     },
-  ]
+  ];
+
+  const todaysDeals = await getProductsByTag({ tag: "todays-deal" });
 
   return (
     <>
-      <HomeCarousel items= { data.carousels} />
-      <div className='md:p-4 md:space-y-4 bg-border'>
+      <HomeCarousel items={data.carousels} />
+      <div className="md:p-4 md:space-y-4 bg-border">
         <HomeCard cards={cards} />
+        <Card className="w-full rounded-none">
+          <CardContent className="p-4 items-center gap-3">
+            <ProductSlider title={"Today's Deals"} products={ todaysDeals} />
+          </CardContent>
+        </Card>
       </div>
     </>
-  )
+  );
 }
+
